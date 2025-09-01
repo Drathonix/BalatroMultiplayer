51aeca0f4759661fec20f431ecdfd4637f58f59b
Stephen Kirk <me@thestephen.net>
2025-09-01 22:01:06 +0200

    photo allows arbitrary hand sizes

--- objects/jokers/sandbox/photograph.lua
+++ objects/jokers/sandbox/photograph.lua
@@ -15,17 +15,22 @@ SMODS.Joker({
 	rarity = 1,
 	cost = 5,
 	pixel_size = { h = 95 / 1.2 },
-	config = { extra = { xmult = 5 }, mp_sticker_balanced = true },
+	config = { extra = { xmult = 4 }, mp_sticker_balanced = true },
 	loc_vars = function(self, info_queue, card)
 		return { vars = { card.ability.extra.xmult } }
 	end,
 	calculate = function(self, card, context)
-		if context.individual and context.cardarea == G.play and context.other_card:is_face() then
-			local is_first_face = false
-			if context.scoring_hand[1]:is_face() then is_first_face = true end
-			if #context.full_hand == 1 and is_first_face then return {
-				xmult = card.ability.extra.xmult,
-			} end
+		if context.individual and context.cardarea == G.play then
+			local face_count = 0
+			for i = 1, #context.scoring_hand do
+				if context.scoring_hand[i]:is_face() then face_count = face_count + 1 end
+			end
+
+			if face_count == 1 and context.other_card:is_face() then
+				return {
+					xmult = card.ability.extra.xmult,
+				}
+			end
 		end
 	end,
 	in_pool = function(self)
